8751fd58c73526988c38c3ee840880737f53a95e
vkulkarni46 <vkulkarni46@gatech.edu>
2024-02-29 22:35:58 -0500

    Fix ubuntu-specific package installation

--- install-platform.js
+++ install-platform.js
@@ -4,7 +4,8 @@ const os = require('os');
 const platform = os.platform();
 
 if (platform === 'linux') {
-  execSync('npm install @esbuild/linux-x64, @tauri-apps/cli-linux-x64-gnu', { stdio: 'inherit' });
+  execSync('npm install @esbuild/linux-x64', { stdio: 'inherit' });
+  execSync('npm install @tauri-apps/cli-linux-x64-gnu', { stdio: 'inherit' });
 } else {
   console.log('Skipping linux-specific-package installation on', platform);
 }
