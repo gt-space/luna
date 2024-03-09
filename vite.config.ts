cca65d2643a9e3036a1f7fae7ecca34adf9db916
Vineet Kulkarni <2vineetk@gmail.com>
2024-03-09 16:02:11 -0500

    Add AVI page to build

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,8 @@ export default defineConfig({
         sensors: resolve(__dirname, 'sensors.html'),
         valves: resolve(__dirname, 'valves.html'),
         plotter: resolve(__dirname, 'plotter.html'),
-        sequences: resolve(__dirname, 'sequences.html')
+        sequences: resolve(__dirname, 'sequences.html'),
+        avi: resolve(__dirname, 'avi.html')
       }
     }
   },
