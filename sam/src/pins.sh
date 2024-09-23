64658ba9122291b179d2523e9e1c9830c682e7ec
oliviabeattie <olivia.beattie@uga.edu>
2024-09-23 13:30:39 -0400

    updated pins script for rev04

--- sam/src/pins.sh
+++ sam/src/pins.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 
 # P8 GPIO
+# config-pin p8.10 gpio
+# config-pin p8.11 gpio
+# config-pin p8.12 gpio
+# config-pin p8.13 gpio
+# config-pin p8.14 gpio
+# config-pin p8.15 gpio
+# config-pin p8.16 gpio
+# config-pin p8.17 gpio
+# config-pin p8.18 gpio
+# config-pin p8.19 gpio
+# config-pin p8.20 gpio
+# config-pin p8.21 gpio
+# config-pin p8.22 gpio
+# config-pin p8.23 gpio
+# config-pin p8.24 gpio
+# config-pin p8.25 gpio
+# config-pin p8.26 gpio
+# config-pin p8.27 gpio
+# config-pin p8.28 gpio
+# config-pin p8.29 gpio
+# config-pin p8.30 gpio
+# config-pin p8.31 gpio
+# config-pin p8.41 gpio
+# config-pin p8.42 gpio
+# config-pin p8.43 gpio
+# config-pin p8.44 gpio
+# config-pin p8.45 gpio
+# config-pin p8.46 gpio
+config-pin p8.7 gpio
+config-pin p8.8 gpio
+config-pin p8.9 gpio
 config-pin p8.10 gpio
 config-pin p8.11 gpio
 config-pin p8.12 gpio
@@ -11,18 +42,12 @@ config-pin p8.16 gpio
 config-pin p8.17 gpio
 config-pin p8.18 gpio
 config-pin p8.19 gpio
-config-pin p8.20 gpio
 config-pin p8.21 gpio
-config-pin p8.22 gpio
-config-pin p8.23 gpio
-config-pin p8.24 gpio
-config-pin p8.25 gpio
-config-pin p8.26 gpio
-config-pin p8.27 gpio
-config-pin p8.28 gpio
-config-pin p8.29 gpio
-config-pin p8.30 gpio
-config-pin p8.31 gpio
+config-pin p8.36 gpio
+config-pin p8.37 gpio
+config-pin p8.38 gpio
+config-pin p8.39 gpio
+config-pin p8.40 gpio
 config-pin p8.41 gpio
 config-pin p8.42 gpio
 config-pin p8.43 gpio
@@ -31,35 +56,53 @@ config-pin p8.45 gpio
 config-pin p8.46 gpio
 
 # P9 GPIO
+# config-pin p9.11 gpio
+# config-pin p9.12 gpio
+# config-pin p9.13 gpio
+# config-pin p9.14 gpio
+# config-pin p9.15 gpio
+# config-pin p9.16 gpio
+# config-pin p9.17 gpio
+# config-pin p9.18 gpio
+# config-pin p9.19 gpio
+# config-pin p9.20 gpio
+# config-pin p9.21 gpio
+# config-pin p9.22 gpio
+# config-pin p9.23 gpio
+# config-pin p9.24 gpio
+# config-pin p9.25 gpio
+# config-pin p9.26 gpio
+# config-pin p9.27 gpio
+# config-pin p9.28 gpio
+# config-pin p9.29 gpio
+# config-pin p9.30 gpio
+# config-pin p9.31 gpio
+# config-pin p9.41 gpio
+# config-pin p9.42 gpio
 config-pin p9.11 gpio
 config-pin p9.12 gpio
-config-pin p9.13 gpio
 config-pin p9.14 gpio
 config-pin p9.15 gpio
 config-pin p9.16 gpio
-config-pin p9.17 gpio
-config-pin p9.18 gpio
-config-pin p9.19 gpio
-config-pin p9.20 gpio
-config-pin p9.21 gpio
-config-pin p9.22 gpio
 config-pin p9.23 gpio
 config-pin p9.24 gpio
 config-pin p9.25 gpio
 config-pin p9.26 gpio
 config-pin p9.27 gpio
-config-pin p9.28 gpio
-config-pin p9.29 gpio
-config-pin p9.30 gpio
-config-pin p9.31 gpio
-config-pin p9.41 gpio
-config-pin p9.42 gpio
+config-pin p9.17 gpio
 
-# SPI
-config-pin p9_17 spi_cs
+# SPI 0 (slow)
+# config-pin p9_17 spi_cs
 config-pin p9_18 spi
 config-pin p9_21 spi
 config-pin p9_22 spi_sclk
+
+# SPI 1 (fast)
+config-pin p9_19 spi_cs
+config-pin p9_28 spi_cs
+config-pin p9_29 spi
+config-pin p9_30 spi
+config-pin p9_31 spi_sclk
 
 # config-pin p9_28 spi_cs
 # config-pin p9_29 spi
