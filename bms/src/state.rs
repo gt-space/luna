e01a54070a1292974f2fa906e49051d974320f6f
georgegoldenbank <guccirhombus1@gmail.com>
2024-10-04 17:57:14 -0400

    Finished init_gpio function to disable everything and pull chip selects low

--- bms/src/state.rs
+++ bms/src/state.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
-use crate::{command, adc::ADCType};
+use crate::adc::ADCKind;
+use crate::{command, adc::ADCKind};
 
 use crate::gpio::{
   Gpio,
@@ -48,24 +49,28 @@ pub fn init_gpio(gpio_controllers: &[Gpio]) {
   command::disable_sam_power(gpio_controllers);
   command::disable_charger(gpio_controllers);
   command::set_estop_low(gpio_controllers);
+
+  for chip_select_pin in get_chip_select_mappings(gpio_controllers).values_mut() {
+    chip_select_pin.digital_write(High); // active low
+  }
 }
 
-pub fn get_chip_select_mappings(gpio_controllers: &[Gpio]) -> HashMap<ADCType, Pin> {
+pub fn get_chip_select_mappings(gpio_controllers: &[Gpio]) -> HashMap<ADCKind, Pin> {
   let mut vbat_umb_charge_chip_select: Pin = gpio_controllers[0].get_pin(30);
   vbat_umb_charge_chip_select.mode(Output);
   let mut sam_and_5v_chip_select: Pin = gpio_controllers[0].get_pin(31);
   sam_and_5v_chip_select.mode(Output);
 
-  HashMap::from([(ADCType::VBatUmbCharge, vbat_umb_charge_chip_select),
-  (ADCType::SamAnd5V, sam_and_5v_chip_select)])
+  HashMap::from([(ADCKind::VBatUmbCharge, vbat_umb_charge_chip_select),
+  (ADCKind::SamAnd5V, sam_and_5v_chip_select)])
 }
 
-pub fn get_drdy_mappings(gpio_controllers: &[Gpio]) -> HashMap<ADCType, Pin> {
+pub fn get_drdy_mappings(gpio_controllers: &[Gpio]) -> HashMap<ADCKind, Pin> {
   let mut vbat_umb_charge_drdy: Pin = gpio_controllers[1].get_pin(28);
   vbat_umb_charge_drdy.mode(Input);
   let mut sam_and_5v_drdy: Pin = gpio_controllers[1].get_pin(18);
   sam_and_5v_drdy.mode(Input);
 
-  HashMap::from([(ADCType::VBatUmbCharge, vbat_umb_charge_drdy), 
-  (ADCType::SamAnd5V, sam_and_5v_drdy)])
+  HashMap::from([(ADCKind::VBatUmbCharge, vbat_umb_charge_drdy), 
+  (ADCKind::SamAnd5V, sam_and_5v_drdy)])
 }
