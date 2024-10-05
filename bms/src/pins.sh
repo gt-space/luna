8766f44928318a3c56abeb61f7ac3e3ccca48380
arascshirwan@gmail.com <arascshirwan@gmail.com>
2024-10-05 10:13:00 -0400

    Change pin.sh

--- bms/src/pins.sh
+++ bms/src/pins.sh
@@ -12,19 +12,19 @@ config-pin p9.24 gpio
 config-pin p9.26 gpio
 
 # P8 GPIO (subtract 46 from the pin number on Altium schematic)
-config-pin p8.7
-config-pin p8.8
-config-pin p8.9
-config-pin p8.10
-config-pin p8.11
-config-pin p8.12
-config-pin p8.13
-config-pin p8.14
-config-pin p8.18
-config-pin p8.19
-config-pin p8.21
-config-pin p8.23
-config-pin p8.30
+config-pin p8.7 gpio
+config-pin p8.8 gpio
+config-pin p8.9 gpio
+config-pin p8.10 gpio
+config-pin p8.11 gpio
+config-pin p8.12 gpio
+config-pin p8.13 gpio
+config-pin p8.14 gpio
+config-pin p8.18 gpio
+config-pin p8.19 gpio
+config-pin p8.21 gpio
+config-pin p8.23 gpio
+config-pin p8.30 gpio
 
 # SPI 0
 config-pin p9_18 spi
