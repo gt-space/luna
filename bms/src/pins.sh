9602b6943e968cfcc3cb9a5b528f9d2c974de57b
georgegoldenbank <guccirhombus1@gmail.com>
2024-10-05 06:13:43 -0400

    made pins.sh script

--- bms/src/pins.sh
+++ bms/src/pins.sh
@@ -1,6 +1,29 @@
 
+# P9 GPIO
+config-pin p9.11 gpio
+config-pin p9.12 gpio
+config-pin p9.13 gpio
+config-pin p9.14 gpio
+config-pin p9.15 gpio
+config-pin p9.16 gpio
+config-pin p9.23 gpio
+config-pin p9.24 gpio
+config-pin p9.26 gpio
 
-
+# P8 GPIO (subtract 46 from the pin number on Altium schematic)
+config-pin p8.7
+config-pin p8.8
+config-pin p8.9
+config-pin p8.10
+config-pin p8.11
+config-pin p8.12
+config-pin p8.13
+config-pin p8.14
+config-pin p8.18
+config-pin p8.19
+config-pin p8.21
+config-pin p8.23
+config-pin p8.30
 
 # SPI 0
 config-pin p9_18 spi
