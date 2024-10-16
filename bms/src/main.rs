c21ca59d167dcfcc9b9d88f3d2d5c2315104e17c
arascshirwan@gmail.com <arascshirwan@gmail.com>
2024-10-16 18:28:05 -0400

    added comments

--- bms/src/main.rs
+++ bms/src/main.rs
@@ -52,18 +52,25 @@ fn main() {
   let mut adcs: Vec<ADC> = vec![adc1, adc2];
   init_adcs(&mut adcs);
 
+  // begin FC communication
   let (data_socket, command_socket, fc_address) = establish_flight_computer_connection();
   
   let mut then = Instant::now();
   loop {
     println!("Checking for commands...");
+
+    // check if commands were sent from the FC. if so, execute them
     check_and_execute(&gpio_controllers, &command_socket);
     println!("Checking heartbeat...");
+
+    // check if the FC is sending a heartbeat. if so, reset the timer
     then = check_heartbeat(&data_socket, then, &gpio_controllers);
     
     let datapoints = poll_adcs(&mut adcs);
 
     println!("Sending data...");
+
+    // send the adc data to the FC
     send_data(&data_socket, &fc_address, datapoints);
   }
 }
@@ -71,16 +78,26 @@ fn main() {
 // make sure you keep track of these UdpSockets, and pass them into the correct
 // functions. Left is data, right is command.
 fn establish_flight_computer_connection() -> (UdpSocket, UdpSocket, SocketAddr) {
+  // area in memory where the flight computer handshake response should be stored
   let mut buf: [u8; 10240] = [0; 10240];
+
+  // create the socket where all the data is send from
   let data_socket = UdpSocket::bind(("0.0.0.0", 4573))
     .expect("Could not open data socket.");
+
+  // create the socket where all the commands are recieved from
   let command_socket = UdpSocket::bind(("0.0.0.0", COMMAND_PORT))
     .expect("Could not open command socket.");
+
+  // make it so the CPU doesn't wait for messages to be recieved
   command_socket.set_nonblocking(true)
     .expect("Could not set command socket to unblocking");
+
+  // make it so the CPU doesn't wait for messages to be sent
   data_socket.set_nonblocking(true)
     .expect("Could not set data socket to unblocking");
 
+  // look for the flight computer based on it's dynamic IP
   let address = format!("{}.local:4573", FC_ADDR)
           .to_socket_addrs()
           .ok()
@@ -94,16 +111,24 @@ fn establish_flight_computer_connection() -> (UdpSocket, UdpSocket, SocketAddr) 
     fc_address.ip()
   );
   
+  // Create the BMS handshake message
+  // It lets the flight computer know know what board type and number this device is.
   let identity = DataMessage::Identity(BMS_ID.to_string());
+
+  // Allocate memory to store the BMS handshake message in 
   let packet = postcard::to_allocvec(&identity)
     .expect("Could not create identity message send buffer");
 
   loop {
+    // Try to send the BMS handshake to the flight computer.
+    // If this panics, it means that the BMS couldn't send the handshake at all.
     let size = data_socket.send_to(&packet, fc_address)
       .expect("Could not send Identity message");
 
     println!("Sent identity of size {size}");
 
+    // Check if the FC has responded with its own handshake message. If so,
+    // convert it from raw bytes to a DataMessage enum
     let result = match data_socket.recv_from(&mut buf) {
       Ok((size, _)) =>
         postcard::from_bytes::<DataMessage>(&buf[..size])
@@ -117,6 +142,7 @@ fn establish_flight_computer_connection() -> (UdpSocket, UdpSocket, SocketAddr) 
     thread::sleep(IDENTITY_WAIT_PERIOD);
 
     match result {
+      // If the Identity message was recieved correctly.
       DataMessage::Identity(id) => {
         println!("Connection established with FC ({id})");
         
@@ -135,8 +161,10 @@ fn establish_flight_computer_connection() -> (UdpSocket, UdpSocket, SocketAddr) 
 }
 
 fn send_data(socket: &UdpSocket, address: &SocketAddr, datapoints: Vec<DataPoint>) {
+  // create a buffer to store the data to send in
   let mut buffer: [u8; 65536] = [0; 65536];
 
+  // get the data and store it in the buffer
   let data = DataMessage::Bms(BMS_ID.to_string(), Cow::Owned(datapoints));
   let seralized = match postcard::to_slice(&data, &mut buffer) {
     Ok(slice) => {
@@ -149,6 +177,7 @@ fn send_data(socket: &UdpSocket, address: &SocketAddr, datapoints: Vec<DataPoint
     }
   };
   
+  // send the data to the FC
   match socket.send_to(seralized, address) {
     Ok(size) => {
       pass!("Successfully sent {size} bytes of data...");
@@ -161,13 +190,16 @@ fn send_data(socket: &UdpSocket, address: &SocketAddr, datapoints: Vec<DataPoint
 
 // Make sure you keep track of the timer that is returned, and pass it in on the next loop
 fn check_heartbeat(socket: &UdpSocket, timer: Instant, gpio_controllers: &[Gpio]) -> Instant {
+  // create a location to store the heartbeat recieved from the FC
   let mut buffer: [u8; 256] = [0; 256];
 
+  // check if we have exceeded the heartbeat timer
   let delta = Instant::now() - timer;
   if delta > HEARTBEAT_TIME_LIMIT {
     abort(gpio_controllers);
   }
 
+  // get data from the socket and insert into buffer
   let size = match socket.recv_from(&mut buffer) {
     Ok((size, _)) => size,
     Err(_) => {
@@ -175,6 +207,7 @@ fn check_heartbeat(socket: &UdpSocket, timer: Instant, gpio_controllers: &[Gpio]
     }
   };
 
+  // convert the recieved data into a DataMessage
   let message = match postcard::from_bytes::<DataMessage>(&buffer[..size]) {
     Ok(message) => message,
     Err(e) => {
@@ -184,8 +217,10 @@ fn check_heartbeat(socket: &UdpSocket, timer: Instant, gpio_controllers: &[Gpio]
   };
 
   match message {
+    // if the message was a Heartbeat, reset the timer
     DataMessage::FlightHeartbeat => Instant::now(),
     _ => {
+      // if not, keep the timer going
       warn!("Expected Flight Heartbeat was not detected.");
       timer
     }
@@ -193,13 +228,16 @@ fn check_heartbeat(socket: &UdpSocket, timer: Instant, gpio_controllers: &[Gpio]
 }
 
 fn check_and_execute(gpio_controllers: &[Gpio], command_socket: &UdpSocket) {
+  // where to store the command recieved from the FC
   let mut buf: [u8; 10240] = [0; 10240];
 
+  // check if we got a command from the FC
   let size = match command_socket.recv_from(&mut buf) {
     Ok((size, _)) => size,
     Err(_) => return,
   };
 
+  // Convert the recieved data into a SamControlMessage
   let command = match postcard::from_bytes::<SamControlMessage>(&buf[..size]) {
     Ok(command) => command,
     Err(e) => {
@@ -209,6 +247,8 @@ fn check_and_execute(gpio_controllers: &[Gpio], command_socket: &UdpSocket) {
   };
 
   pass!("Executing command...");
+  
+  // execute the command
   execute(gpio_controllers, command);
 }
 
