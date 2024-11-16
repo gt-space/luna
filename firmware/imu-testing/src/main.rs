77ca5c296cf4abcb86fc486a15432fbb479899c7
AndrewCarlisleECE <andrewcarlisleece@gmail.com>
2024-11-15 19:18:45 -0500

    Implemented static initialization for GPIO as seen in BMS

--- firmware/imu-testing/src/main.rs
+++ firmware/imu-testing/src/main.rs
@@ -2,6 +2,7 @@ use imu::{AdisIMUDriver, DeltaReadData, GenericData, GyroReadData};
 use common::comm::gpio::{Gpio, Pin, PinMode, PinMode::*, PinValue::*};
 use spidev::Spidev;
 use std::sync::Mutex;
+use once_cell::sync::Lazy;
 
 use std::time::Duration;
 use std::time::SystemTime;
@@ -14,28 +15,28 @@ const MAG_CS_PIN_LOC : [usize; 2] = [1, 14];
 const IMU_DR_PIN_LOC : [usize; 2] = [2, 17];
 const IMU_NRESET_PIN_LOC : [usize; 2] = [2, 25];
 
+pub static GPIO_CONTROLLERS: Lazy<Vec<Gpio>> = Lazy::new(|| open_controllers());
+
+// controller = floor(GPIO#/32)
+// pin = remainder
+
+pub fn open_controllers() -> Vec<Gpio> {
+  (0..=3).map(Gpio::open_controller).collect()
+}
 
 fn main() {
   env::set_var("RUST_BACKTRACE", "1");
   println!("Getting GPIO and pins");
-  // Get GPIO handlers
-  static mut CONTROLLERS : [Gpio; 4] = [
-    Gpio::open_controller(0),
-    Gpio::open_controller(1),
-    Gpio::open_controller(2),
-    Gpio::open_controller(3),
-  ];
-  
 
-  let mut imu_cs = unsafe { CONTROLLERS[IMU_CS_PIN_LOC[0]].get_pin(IMU_CS_PIN_LOC[1]) };
+  let mut imu_cs = GPIO_CONTROLLERS[IMU_CS_PIN_LOC[0]].get_pin(IMU_CS_PIN_LOC[1]);
   imu_cs.mode(Output);
-  let mut bar_cs = unsafe { CONTROLLERS[BAR_CS_PIN_LOC[0]].get_pin(BAR_CS_PIN_LOC[1]) };
+  let mut bar_cs = GPIO_CONTROLLERS[BAR_CS_PIN_LOC[0]].get_pin(BAR_CS_PIN_LOC[1]);
   bar_cs.mode(Output);
-  let mut mag_cs = unsafe { CONTROLLERS[MAG_CS_PIN_LOC[0]].get_pin(MAG_CS_PIN_LOC[1]) };
+  let mut mag_cs = GPIO_CONTROLLERS[MAG_CS_PIN_LOC[0]].get_pin(MAG_CS_PIN_LOC[1]);
   mag_cs.mode(Output);
-  let mut imu_dr = unsafe { CONTROLLERS[IMU_DR_PIN_LOC[0]].get_pin(IMU_DR_PIN_LOC[1]) };
+  let mut imu_dr = GPIO_CONTROLLERS[IMU_DR_PIN_LOC[0]].get_pin(IMU_DR_PIN_LOC[1]);
   imu_dr.mode(Input);
-  let mut imu_nreset = unsafe { CONTROLLERS[IMU_NRESET_PIN_LOC[0]].get_pin(IMU_NRESET_PIN_LOC[1]) };
+  let mut imu_nreset = GPIO_CONTROLLERS[IMU_NRESET_PIN_LOC[0]].get_pin(IMU_NRESET_PIN_LOC[1]);
   imu_nreset.mode(Output);
   
   // Ensure all CS are off
