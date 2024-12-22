9173df9147d8f50984b5fbeb8ba489ebc774843c
georgegoldenbank <guccirhombus1@gmail.com>
2024-12-22 17:41:07 -0500

    Got working transition from 1 state to another (sleep to standby)

--- firmware/sx1280/src/state.rs
+++ firmware/sx1280/src/state.rs
@@ -103,6 +103,10 @@ impl SX1280<StandbyData> {
           }
       }
 
+      dio_1.mode(Input);
+      dio_2.mode(Input);
+      dio_3.mode(Input);
+
       let driver = SX1280 { 
         state: StandbyData {
           spidev,
@@ -129,17 +133,55 @@ impl SX1280<StandbyData> {
       self.state.cs_pin.digital_write(High); // active low
     }
 
-    pub fn set_regulator_mode(&self, mode: u8) -> io::Result<()> {
-      if (mode != 0 || mode != 1) {
+    pub fn set_regulator_mode(&mut self, mode: u8) -> io::Result<()> {
+      if (mode != 0 && mode != 1) {
         // fuck you
       }
       
       self.enable_chip_select();
-      let tx: [u8; 2] = [Command::SetRegulatorMode, mode];
+      let tx: [u8; 2] = [Command::SetRegulatorMode as u8, mode];
       let mut transfer = SpidevTransfer::write(&tx);
       let result = self.state.spidev.transfer(&mut transfer);
       self.disable_chip_select();
 
       result
     }
+}
+
+impl SX1280<SleepData> {
+  pub fn set_standby(mut self, regulator_mode: u8) -> SX1280<StandbyData> {
+    self.enable_chip_select();
+    thread::sleep(Duration::from_micros(2)); // delay for minimum 2 us
+    self.disable_chip_select();
+
+    loop {
+      if self.state.busy_pin.digital_read() == Low {
+        break;
+      }
+    }
+
+    let mut driver = SX1280 { 
+      state: StandbyData { 
+        spidev: self.state.spidev,
+        cs_pin: self.state.cs_pin,
+        busy_pin: self.state.busy_pin,
+        reset_pin: self.state.reset_pin,
+        dio_1: self.state.dio_1,
+        dio_2: self.state.dio_2,
+        dio_3: self.state.dio_3
+      }
+    };
+
+    driver.set_regulator_mode(regulator_mode).unwrap();
+
+    driver
+  }
+
+  pub fn enable_chip_select(&mut self) {
+    self.state.cs_pin.digital_write(Low); // active low
+  }
+
+  pub fn disable_chip_select(&mut self) {
+    self.state.cs_pin.digital_write(High); // active low
+  }
 }
