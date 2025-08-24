741acfb46865b122ea9e52110cf5c9783b66573b
pchryss <philipchryssochoos@gmail.com>
2025-08-24 12:26:48 -0400

    Update comms get() and .h

--- reco/communications.c
+++ reco/communications.c
@@ -2,27 +2,40 @@
 
 // Need something to define the data transmission rate/port/pin for init
 
-void getFlow(baro_handle_t* baro, spi_device_t* baroSPI) {
-  getCurrTempPressure(baroSPI, baro);
+bool getFlow(baro_handle_t* baro, spi_device_t* baroSPI) {
+  baro_status_t baroResp = getCurrTempPressure(baroSPI, baro);
+  return baroResp == BARO_COMMS_OK;
 }
 
-heading_data_t getHeading(mag_handler_t* mag, spi_device_t* magSPI) {
-  heading_data_t heading_data;
-  heading_data.xMag = getXMag(magSPI, mag);
-  heading_data.yMag = getYMag(magSPI, mag);
-  heading_data.zMag = getZMag(magSPI, mag);
-  return heading_data;
+bool getHeading(mag_handler_t* mag, spi_device_t* magSPI, heading_data_t* heading_data) {
+  mag_status_t xMagResp = lis3mdl_get_x_mag(magSPI, mag, &heading_data->xMag);
+  mag_status_t yMagResp = lis3mdl_get_y_mag(magSPI, mag, &heading_data->yMag);
+  mag_status_t zMagResp = lis3mdl_get_z_mag(magSPI, mag, &heading_data->zMag);
+  if (xMagResp != IMU_COMMS_OK ||
+      yMagResp != IMU_COMMS_OK ||
+      zMagResp != IMU_COMMS_OK) {
+      return false;
+  }
+  return true;
 }
 
-acceleration_data_t getAcceleration(imu_handler_t* imu, spi_device_t* imuSPI) {
-  acceleration_data_t acceleration_data;
-  getXAccel(imuSPI, imu, acceleration_data.xActualAccel);
-  getYAccel(imuSPI, imu, acceleration_data.yActualAccel);
-  getZAccel(imuSPI, imu, acceleration_data.zActualAccel);
-  getPitch(imuSPI, imu, acceleration_data.pitch);
-  getRoll(imuSPI, imu, acceleration_data.roll);
-  getYaw(imuSPI, imu, acceleration_data.yaw);
-  return acceleration_data;
+bool getAcceleration(imu_handler_t* imu, spi_device_t* imuSPI, acceleration_data_t* acceleration_data) {
+  imu_status_t xAccelResp = getXAccel(imuSPI, imu, &acceleration_data->xActualAccel);
+  imu_status_t yAccelResp = getYAccel(imuSPI, imu, &acceleration_data->yActualAccel);
+  imu_status_t zAccelResp = getZAccel(imuSPI, imu, &acceleration_data->zActualAccel);
+  imu_status_t pitchResp = getPitch(imuSPI, imu, &acceleration_data->pitch);
+  imu_status_t rollResp = getRoll(imuSPI, imu, &acceleration_data->roll);
+  imu_status_t yawResp = getYaw(imuSPI, imu, &acceleration_data->yaw);
+
+  if (xAccelResp != IMU_COMMS_OK ||
+      yAccelResp != IMU_COMMS_OK ||
+      zAccelResp != IMU_COMMS_OK ||
+      pitchResp  != IMU_COMMS_OK ||
+      rollResp   != IMU_COMMS_OK ||
+      yawResp    != IMU_COMMS_OK) {
+      return false;
+  }
+  return true;
 }
 
 // TBD
--- reco/communications.h
+++ reco/communications.h
@@ -90,11 +90,11 @@ typedef struct {
 
 // Need something to define the data transmission rate/port/pin for init
 
-void getFlow();
+bool getFlow(baro_handle_t* baro, spi_device_t* baroSPI);
 
-heading_data_t getHeading();
+bool getHeading(mag_handler_t* mag, spi_device_t* magSPI, heading_data_t* heading_data);
 
-acceleration_data_t getAcceleration();
+bool getAcceleration(imu_handler_t* imu, spi_device_t* imuSPI, acceleration_data_t* acceleration_data);
 
 // TBD
 double getLocation();
