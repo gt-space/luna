ceb807923b8cddb5c1e38a24555a47eaa07eaeb4
Isaac Hung <manhei5001@gmail.com>
2025-10-21 22:56:13 -0400

    remove LIS3MDL & add pins script for LIS2MDL

---
+++ firmware/lis2mdl/pins_fixed.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# P8 GPIO
+config-pin p8.34 gpio # IMU-NRESET
+config-pin p8.30 gpio # IMU-DR
+config-pin p8.46 gpio # CAM-EN
+
+# P9 GPIO
+
+# SPI1
+config-pin p9.31 spi_sclk
+config-pin p9.29 spi
+config-pin p9.30 spi
+config-pin p9.19 spi_cs # MAG-CS
+config-pin p9.20 spi_cs # BAR-CS
+
+# SPI0
+config-pin p9.22 spi_sclk
+config-pin p9.21 spi
+config-pin p9.18 spi
+config-pin p9.17 spi_cs # IMU-CS
