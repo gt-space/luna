14c0d4115e69668bb14fe487142da33b5f6ec0c0
Gabriel Ashkenazi <gabriel.ashkenazi1@gmail.com>
2025-10-30 17:14:31 -0400

    added functions compute_lladot, Compute_vdot, compute_Pqdot, compute_g_dg, update_GPS

--- reco/EKF/compute_common.c
+++ reco/EKF/compute_common.c
@@ -2,7 +2,8 @@
 
 // attitude, pos, vel, g bias, a bias, g sf, a sf
 // x0 = [q0; lla0; zeros(3,1); zeros(3,1); zeros(3,1); zeros(3,1); zeros(3,1)];
-inline void getStateQuaternion(arm_matrix_instance_f32* x, arm_matrix_instance_f32* quaternion, float32_t* quaternionData) {
+inline void getStateQuaternion(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *quaternion, float32_t *quaternionData)
+{
 	quaternionData[0] = x->pData[0];
 	quaternionData[1] = x->pData[1];
 	quaternionData[2] = x->pData[2];
@@ -11,7 +12,8 @@ inline void getStateQuaternion(arm_matrix_instance_f32* x, arm_matrix_instance_f
 	arm_mat_init_f32(quaternion, 4, 1, quaternionData);
 }
 
-inline void getStatePosition(arm_matrix_instance_f32* x, arm_matrix_instance_f32* position, float32_t* posData) {
+inline void getStatePosition(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *position, float32_t *posData)
+{
 	posData[0] = x->pData[4];
 	posData[1] = x->pData[5];
 	posData[2] = x->pData[6];
@@ -19,7 +21,8 @@ inline void getStatePosition(arm_matrix_instance_f32* x, arm_matrix_instance_f32
 	arm_mat_init_f32(position, 3, 1, posData);
 }
 
-inline void getStateVelocity(arm_matrix_instance_f32* x, arm_matrix_instance_f32* vel, float32_t* velData) {
+inline void getStateVelocity(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *vel, float32_t *velData)
+{
 	velData[0] = x->pData[7];
 	velData[1] = x->pData[8];
 	velData[2] = x->pData[9];
@@ -27,7 +30,8 @@ inline void getStateVelocity(arm_matrix_instance_f32* x, arm_matrix_instance_f32
 	arm_mat_init_f32(vel, 3, 1, velData);
 }
 
-inline void getStateGBias(arm_matrix_instance_f32* x, arm_matrix_instance_f32* gBias, float32_t* gData) {
+inline void getStateGBias(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *gBias, float32_t *gData)
+{
 	gData[0] = x->pData[10];
 	gData[1] = x->pData[11];
 	gData[2] = x->pData[12];
@@ -35,7 +39,8 @@ inline void getStateGBias(arm_matrix_instance_f32* x, arm_matrix_instance_f32* g
 	arm_mat_init_f32(gBias, 3, 1, gData);
 }
 
-inline void getStateABias(arm_matrix_instance_f32* x, arm_matrix_instance_f32* aBias, float32_t* aData) {
+inline void getStateABias(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *aBias, float32_t *aData)
+{
 	aData[0] = x->pData[13];
 	aData[1] = x->pData[14];
 	aData[2] = x->pData[15];
@@ -43,7 +48,8 @@ inline void getStateABias(arm_matrix_instance_f32* x, arm_matrix_instance_f32* a
 	arm_mat_init_f32(aBias, 3, 1, aData);
 }
 
-inline void getStateGSF(arm_matrix_instance_f32* x, arm_matrix_instance_f32* g_sf, float32_t* g_sf_data) {
+inline void getStateGSF(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *g_sf, float32_t *g_sf_data)
+{
 	g_sf_data[0] = x->pData[16];
 	g_sf_data[1] = x->pData[17];
 	g_sf_data[2] = x->pData[18];
@@ -51,7 +57,8 @@ inline void getStateGSF(arm_matrix_instance_f32* x, arm_matrix_instance_f32* g_s
 	arm_mat_init_f32(g_sf, 3, 1, g_sf_data);
 }
 
-inline void getStateASF(arm_matrix_instance_f32* x, arm_matrix_instance_f32* a_sf, float32_t* a_sf_data) {
+inline void getStateASF(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *a_sf, float32_t *a_sf_data)
+{
 	a_sf_data[0] = x->pData[19];
 	a_sf_data[1] = x->pData[20];
 	a_sf_data[2] = x->pData[21];
@@ -60,58 +67,60 @@ inline void getStateASF(arm_matrix_instance_f32* x, arm_matrix_instance_f32* a_s
 }
 
 // Doesn't implement the check to make sure that is 1x4 vector (lines 6 through 9 in the respective .m file)
-void quaternion2DCM(const arm_matrix_instance_f32* quaternion, arm_matrix_instance_f32* CB2I, float32_t* CB2IBuffer) {
-    // Matrices and buffers
-    arm_matrix_instance_f32 qNormMat, scalarQ, vectorQ;
-    arm_matrix_instance_f32 identityMat, eyeDot, outerProduct, skewMat, term1;
+void quaternion2DCM(const arm_matrix_instance_f32 *quaternion, arm_matrix_instance_f32 *CB2I, float32_t *CB2IBuffer)
+{
+	// Matrices and buffers
+	arm_matrix_instance_f32 qNormMat, scalarQ, vectorQ;
+	arm_matrix_instance_f32 identityMat, eyeDot, outerProduct, skewMat, term1;
 
-    // Normalize quaternion
-    float32_t qNorm[4];
-    arm_quaternion_normalize_f32(quaternion->pData, qNorm, 1);
-    arm_mat_init_f32(&qNormMat, 4, 1, qNorm);
+	// Normalize quaternion
+	float32_t qNorm[4];
+	arm_quaternion_normalize_f32(quaternion->pData, qNorm, 1);
+	arm_mat_init_f32(&qNormMat, 4, 1, qNorm);
 
-    // Extract scalar (s) and vector (v)
-    float32_t scalarVal, vectorVal[3];
-    arm_quaternion_scalar_f32(&qNormMat, &scalarQ, &scalarVal);
-    arm_quaternion_vector_f32(&qNormMat, &vectorQ, vectorVal);
+	// Extract scalar (s) and vector (v)
+	float32_t scalarVal, vectorVal[3];
+	arm_quaternion_scalar_f32(&qNormMat, &scalarQ, &scalarVal);
+	arm_quaternion_vector_f32(&qNormMat, &vectorQ, vectorVal);
 
-    // Compute v·v
-    float32_t vDotProd;
-    arm_dot_prod_f32(vectorVal, vectorVal, 3, &vDotProd);
+	// Compute v·v
+	float32_t vDotProd;
+	arm_dot_prod_f32(vectorVal, vectorVal, 3, &vDotProd);
 
-    // Identity matrix I
-    float32_t identityData[9];
-    arm_mat_eye_f32(&identityMat, identityData, 3);
+	// Identity matrix I
+	float32_t identityData[9];
+	arm_mat_eye_f32(&identityMat, identityData, 3);
 
-    // (s^2 - dot(v,v)) * I
-    float32_t eyeDotData[9];
-    arm_mat_init_f32(&eyeDot, 3, 3, eyeDotData);
-    float32_t firstPart = scalarVal * scalarVal - vDotProd;
-    arm_mat_scale_f32(&identityMat, firstPart, &eyeDot);
+	// (s^2 - dot(v,v)) * I
+	float32_t eyeDotData[9];
+	arm_mat_init_f32(&eyeDot, 3, 3, eyeDotData);
+	float32_t firstPart = scalarVal * scalarVal - vDotProd;
+	arm_mat_scale_f32(&identityMat, firstPart, &eyeDot);
 
-    // 2 * v * v'
-    float32_t outerProductData[9];
-    arm_mat_outer_product_f32(&vectorQ, &outerProduct, outerProductData);
-    arm_mat_scale_f32(&outerProduct, 2.0f, &outerProduct);
+	// 2 * v * v'
+	float32_t outerProductData[9];
+	arm_mat_outer_product_f32(&vectorQ, &outerProduct, outerProductData);
+	arm_mat_scale_f32(&outerProduct, 2.0f, &outerProduct);
 
-    // 2 * s * skew(v)
-    float32_t skewData[9];
-    arm_mat_skew_f32(&skewMat, vectorVal, skewData);
-    arm_mat_scale_f32(&skewMat, 2.0f * scalarVal, &skewMat);
+	// 2 * s * skew(v)
+	float32_t skewData[9];
+	arm_mat_skew_f32(&skewMat, vectorVal, skewData);
+	arm_mat_scale_f32(&skewMat, 2.0f * scalarVal, &skewMat);
 
-    // Sum: CB2I = (s^2 - v·v)I + 2vv' + 2s*skew(v)
-    float32_t term1Data[9];
-    arm_mat_init_f32(&term1, 3, 3, term1Data);
-    arm_mat_add_f32(&eyeDot, &outerProduct, &term1);
+	// Sum: CB2I = (s^2 - v·v)I + 2vv' + 2s*skew(v)
+	float32_t term1Data[9];
+	arm_mat_init_f32(&term1, 3, 3, term1Data);
+	arm_mat_add_f32(&eyeDot, &outerProduct, &term1);
 
-    arm_mat_init_f32(CB2I, 3, 3, CB2IBuffer);
-    arm_mat_add_f32(&term1, &skewMat, CB2I);
-    return;
+	arm_mat_init_f32(CB2I, 3, 3, CB2IBuffer);
+	arm_mat_add_f32(&term1, &skewMat, CB2I);
+	return;
 }
 
-void compute_radii(float32_t phi, float32_t* returnVector) {
-	float32_t a = 6378137; // semi-major axis
-	float32_t b = 6356752.31425; // semi-minor axis
+void compute_radii(float32_t phi, float32_t *returnVector)
+{
+	float32_t a = 6378137;					 // semi-major axis
+	float32_t b = 6356752.31425;			 // semi-minor axis
 	float32_t ecc = 1 - ((a / b) * (a / b)); // eccentricity
 
 	float32_t num1 = a * (1 - ecc);
@@ -137,8 +146,3 @@ void compute_radii(float32_t phi, float32_t* returnVector) {
 	returnVector[2] = dR_phi_dphi;
 	returnVector[3] = dR_phi_dphi;
 }
-
-void compute_g_dg(arm_matrix_instance_f32* x, float32_t* result) {
-
-}
-
--- reco/EKF/compute_common.h
+++ reco/EKF/compute_common.h
@@ -6,15 +6,15 @@
 #include "cmsis_dsp_extensions/quaternion_extensions.h"
 #include "cmsis_dsp_extensions/trig_extensions.h"
 
-inline void getStateQuaternion(arm_matrix_instance_f32* x, arm_matrix_instance_f32* quaternion, float32_t* quaternionData);
-inline void getStatePosition(arm_matrix_instance_f32* x, arm_matrix_instance_f32* position, float32_t* posData);
-inline void getStateVelocity(arm_matrix_instance_f32* x, arm_matrix_instance_f32* vel, float32_t* velData);
-inline void getStateGBias(arm_matrix_instance_f32* x, arm_matrix_instance_f32* gBias, float32_t* gData);
-inline void getStateABias(arm_matrix_instance_f32* x, arm_matrix_instance_f32* aBias, float32_t* aData);
-inline void getStateGSF(arm_matrix_instance_f32* x, arm_matrix_instance_f32* g_sf, float32_t* g_sf_data);
-inline void getStateASF(arm_matrix_instance_f32* x, arm_matrix_instance_f32* a_sf, float32_t* a_sf_data);
-void quaternion2DCM(const arm_matrix_instance_f32* quaternion, arm_matrix_instance_f32* CB2I, float32_t* CB2IBuffer);
-void compute_radii(float32_t phi, float32_t* returnVector);
-void compute_g_dg();
+inline void getStateQuaternion(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *quaternion, float32_t *quaternionData);
+inline void getStatePosition(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *position, float32_t *posData);
+inline void getStateVelocity(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *vel, float32_t *velData);
+inline void getStateGBias(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *gBias, float32_t *gData);
+inline void getStateABias(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *aBias, float32_t *aData);
+inline void getStateGSF(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *g_sf, float32_t *g_sf_data);
+inline void getStateASF(arm_matrix_instance_f32 *x, arm_matrix_instance_f32 *a_sf, float32_t *a_sf_data);
+void quaternion2DCM(const arm_matrix_instance_f32 *quaternion, arm_matrix_instance_f32 *CB2I, float32_t *CB2IBuffer);
+void compute_radii(float32_t phi, float32_t *returnVector);
+void compute_g_dg(float32_t phi, float32_t h, float32_t *dg_dphi, float32_t *dg_dh);
 
 #endif
