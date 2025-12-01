f3084418de81ea966df386523b343aa58d9737ce
Isaac Hung <manhei5001@gmail.com>
2025-12-01 18:46:07 -0500

    move IMU to separate thread

--- ahrs/src/state.rs
+++ ahrs/src/state.rs
@@ -1,6 +1,8 @@
 use std::net::{SocketAddr, UdpSocket};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{mpsc, Arc};
 use std::thread;
-use std::time::{Duration, Instant, SystemTime};
+use std::time::{Instant, SystemTime};
 
 use crate::adc::{read_3v3_rail, read_5v_rail};
 use crate::communication::{
@@ -15,20 +17,10 @@ use jeflog::fail;
 use lis2mdl::LIS2MDL;
 use ms5611::MS5611;
 
-const FREQUENCY: f64 = 300.0;
-const INTERVAL: f64 = 1.0 / FREQUENCY;
-
 pub enum State {
   Init,
-  Connect(ConnectData),
   MainLoop(MainLoopData),
-  Abort(AbortData),
-}
-
-pub struct ConnectData {
-  imu: AdisIMUDriver,
-  barometer: MS5611,
-  magnetometer: LIS2MDL,
+  Abort,
 }
 
 pub struct MainLoopData {
@@ -36,25 +28,17 @@ pub struct MainLoopData {
   my_command_socket: UdpSocket,
   fc_address: SocketAddr,
   then: Instant,
-  imu: AdisIMUDriver,
+  imu_thread: (thread::JoinHandle<()>, Arc<AtomicBool>, mpsc::Receiver<Imu>),
   barometer: MS5611,
   magnetometer: LIS2MDL,
-}
-
-pub struct AbortData {
-  reconnect_data: ConnectData,
 }
 
 impl State {
   pub fn next(self) -> Self {
     match self {
       State::Init => init(),
-
-      State::Connect(data) => connect(data),
-
       State::MainLoop(data) => main_loop(data),
-
-      State::Abort(data) => abort(data),
+      State::Abort => abort(),
     }
   }
 }
@@ -64,31 +48,56 @@ fn init() -> State {
   init_gpio(); // pull all chip selects high
 
   println!("Initializing drivers");
+  let mut imu = init_imu().expect("failed to initialize IMU driver");
+  let barometer =
+    init_barometer().expect("failed to initialize barometer driver");
+  let magnetometer =
+    init_magnetometer().expect("failed to initialize magnetometer driver");
 
-  State::Connect(ConnectData {
-    imu: init_imu().expect("failed to initialize IMU driver"),
-    barometer: init_barometer().expect("failed to initialize barometer driver"),
-    magnetometer: init_magnetometer()
-      .expect("failed to initialize magnetometer driver"),
-  })
-}
-
-fn connect(data: ConnectData) -> State {
   println!("Connecting to flight computer");
-
   let (data_socket, command_socket, fc_address) =
     establish_flight_computer_connection();
-
   println!("Connected to: {}", fc_address);
+
+  let running = Arc::new(AtomicBool::new(true));
+  let (imu_tx, imu_rx) = mpsc::channel();
+  let imu_thread_handle = {
+    let running = running.clone();
+    thread::spawn(move || {
+      while running.load(Ordering::SeqCst) {
+        match imu.burst_read_gyro_16() {
+          Ok((_, imu_data)) => {
+            let (accel, gyro) =
+              (imu_data.get_accel_float(), imu_data.get_gyro_float());
+            let data = Imu {
+              accelerometer: Vector {
+                x: accel[0] as f64,
+                y: accel[1] as f64,
+                z: accel[2] as f64,
+              },
+              gyroscope: Vector {
+                x: gyro[0] as f64,
+                y: gyro[1] as f64,
+                z: gyro[2] as f64,
+              },
+            };
+            dbg!(data);
+            imu_tx.send(data).expect("main thread has already exited");
+          }
+          Err(e) => fail!("Failed to read IMU data: {e}"),
+        };
+      }
+    })
+  };
 
   State::MainLoop(MainLoopData {
     my_data_socket: data_socket,
     my_command_socket: command_socket,
     fc_address,
     then: Instant::now(),
-    imu: data.imu,
-    barometer: data.barometer,
-    magnetometer: data.magnetometer,
+    imu_thread: (imu_thread_handle, running, imu_rx),
+    barometer: barometer,
+    magnetometer: magnetometer,
   })
 }
 
@@ -97,40 +106,16 @@ fn main_loop(mut data: MainLoopData) -> State {
   let (updated_time, abort_status) =
     check_heartbeat(&data.my_data_socket, data.then);
   data.then = updated_time;
-  let loop_start = Instant::now();
 
   if abort_status {
-    return State::Abort(AbortData {
-      reconnect_data: ConnectData {
-        imu: data.imu,
-        barometer: data.barometer,
-        magnetometer: data.magnetometer,
-      },
-    });
+    let (handle, running, _) = data.imu_thread;
+    running.store(false, Ordering::SeqCst);
+    handle.join().expect("IMU thread panicked");
+    return State::Abort;
   }
 
-  let imu = match data.imu.burst_read_gyro_16() {
-    Ok((_, imu_data)) => {
-      let (accel, gyro) =
-        (imu_data.get_accel_float(), imu_data.get_gyro_float());
-      Imu {
-        accelerometer: Vector {
-          x: accel[0] as f64,
-          y: accel[1] as f64,
-          z: accel[2] as f64,
-        },
-        gyroscope: Vector {
-          x: gyro[0] as f64,
-          y: gyro[1] as f64,
-          z: gyro[2] as f64,
-        },
-      }
-    }
-    Err(e) => {
-      fail!("Failed to read IMU data: {e}");
-      return State::MainLoop(data);
-    }
-  };
+  let (_, _, imu_rx) = &data.imu_thread;
+  let imu = imu_rx.recv().expect("IMU thread already exited");
 
   let barometer = match (
     data.barometer.read_temperature(),
@@ -176,18 +161,13 @@ fn main_loop(mut data: MainLoopData) -> State {
 
   send_data(&data.my_data_socket, &data.fc_address, datapoint);
 
-  thread::sleep(
-    Duration::from_secs_f64(INTERVAL)
-      .saturating_sub(Instant::now().duration_since(loop_start)),
-  );
-
   State::MainLoop(data)
 }
 
-fn abort(data: AbortData) -> State {
+fn abort() -> State {
   fail!("Aborting goodbye!");
 
   init_gpio(); // pull all chip selects high
 
-  State::Connect(data.reconnect_data)
+  State::Init
 }
