774bee342f24ec74c9b7e842c8b65b9dde726a9a
Aras Shirwan <ashirwan3@gatech.edu>
2025-12-19 15:29:38 -0600

    Added FTel test module.

--- flight2/src/ftel.rs
+++ flight2/src/ftel.rs
@@ -14,14 +14,14 @@ impl FtelSocket {
   pub fn init(address: impl ToSocketAddrs, update_rate: Duration)
   -> Result<FtelSocket> {
     let Some(address) = address.to_socket_addrs()?.next() else {
-      return Err(FtelSocketError::Resolution);
+      return Err(Error::Resolution);
     };
 
     let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
     socket.bind(&SockAddr::from(address))?;
     socket.set_nonblocking(true)?;
     socket.set_tos(FTEL_DSCP << 2)?;
-    Ok(Self { 
+    Ok(Self {
       socket,
       last_sent: None,
       update_rate,
@@ -84,15 +84,12 @@ impl FtelSocket {
     // computes the total number of packets which need to be sent for this
     // VehicleState, which includes the XOR packet and any overfill packets for
     // VehicleStates whose length is not divisible by 255.
-    let mut total_packets = (state_bytes.len() / FTEL_PACKET_PAYLOAD_LENGTH + 1) as u8;
-    if state_bytes.len() % FTEL_PACKET_PAYLOAD_LENGTH != 0 {
-      total_packets += 1;
-    }
+    let total_packets = state_bytes.len().div_ceil(FTEL_PACKET_PAYLOAD_LENGTH) as u8 + 1;
 
     buf[0] = self.messages_sent as u8;
     buf[2] = total_packets;
-    &buf[3..=4].copy_from_slice(&u16::to_be_bytes(state_bytes.len() as u16));
-    &xor_buf[0..FTEL_PACKET_METADATA_LENGTH].copy_from_slice(&buf[0..FTEL_PACKET_METADATA_LENGTH]);
+    buf[3..=4].copy_from_slice(&u16::to_be_bytes(state_bytes.len() as u16));
+    xor_buf[0..FTEL_PACKET_METADATA_LENGTH].copy_from_slice(&buf[0..FTEL_PACKET_METADATA_LENGTH]);
     xor_buf[1] = total_packets - 1;
 
     // Compute XOR packet.
@@ -106,12 +103,12 @@ impl FtelSocket {
 
       // Copy over a slice of the message to the buffer.
       // Will panic if the slice lengths don't match. Scary!
-      &buf[FTEL_PACKET_METADATA_LENGTH..].copy_from_slice(&state_bytes[current_packet as usize * FTEL_PACKET_PAYLOAD_LENGTH..current_packet as usize * FTEL_PACKET_PAYLOAD_LENGTH + payload_length]);
+      buf[FTEL_PACKET_METADATA_LENGTH..].copy_from_slice(&state_bytes[current_packet as usize * FTEL_PACKET_PAYLOAD_LENGTH..current_packet as usize * FTEL_PACKET_PAYLOAD_LENGTH + payload_length]);
 
       let packet_length = payload_length as usize + FTEL_PACKET_METADATA_LENGTH;
       let bytes_written = self.socket.send_to(&buf[..packet_length], &dest_addr)?;
       if bytes_written != packet_length {
-        return Err(FtelSocketError::SocketWrite(bytes_written, packet_length));
+        return Err(Error::SocketWrite(bytes_written, packet_length));
       }
 
       current_packet += 1;
@@ -121,16 +118,17 @@ impl FtelSocket {
     // Send the XOR Packet
     let bytes_written = self.socket.send_to(&xor_buf, &dest_addr)?;
     if bytes_written != xor_buf.len() {
-      return Err(FtelSocketError::SocketWrite(bytes_written, xor_buf.len()));
+      return Err(Error::SocketWrite(bytes_written, xor_buf.len()));
     }
     
     Ok(())
   }
 }
 
-type Result<T> = ::std::result::Result<T, FtelSocketError>;
+type Result<T> = ::std::result::Result<T, Error>;
 
-pub enum FtelSocketError {
+#[derive(Debug)]
+pub enum Error {
   /// There was an issue trying to resolve the socket address to bind to.
   Resolution,
   /// There was an issue with the underlying UDP socket.
@@ -144,19 +142,19 @@ pub enum FtelSocketError {
   SocketWrite(usize, usize),
 }
 
-impl From<io::Error> for FtelSocketError {
+impl From<io::Error> for Error {
   fn from(error: io::Error) -> Self {
-    FtelSocketError::Transport(error)
+    Error::Transport(error)
   }
 }
 
-impl From<postcard::Error> for FtelSocketError {
+impl From<postcard::Error> for Error {
   fn from(error: postcard::Error) -> Self {
-    FtelSocketError::Postcard(error)
+    Error::Postcard(error)
   }
 }
 
-impl fmt::Display for FtelSocketError {
+impl fmt::Display for Error {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     match self {
       Self::Resolution => write!(f, "Couldn't resolve binding socket address."),
@@ -164,5 +162,47 @@ impl fmt::Display for FtelSocketError {
       Self::Postcard(e) => write!(f, "Postcard error: {e}"),
       Self::Transport(e) => write!(f, "Transport error: {e}")
     }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use std::{mem::MaybeUninit, sync::atomic::{AtomicI32, Ordering}};
+  static IDENTIFIER: AtomicI32 = AtomicI32::new(4573);
+  
+  fn initalize(duration: Duration) -> (FtelSocket, SocketAddr, Socket) {
+    let identifier = IDENTIFIER.fetch_add(2, Ordering::Relaxed);
+
+    let address = format!("127.0.0.1:{}", identifier + 1).to_socket_addrs().unwrap().next().unwrap();
+    let ftel = FtelSocket::init(address, duration).unwrap();
+
+    let mocket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP)).unwrap();
+    let address = format!("127.0.0.1:{identifier}").to_socket_addrs().unwrap().next().unwrap();
+    mocket.bind(&SockAddr::from(address)).unwrap();
+    mocket.set_nonblocking(true).unwrap();
+
+    (ftel, address, mocket)
+  }
+
+  #[test]
+  fn empty_message() {
+    let (mut ftel, dest, mocket) = initalize(Duration::ZERO);
+
+    ftel.reverse_poll(&dest, &()).unwrap();
+    let mut buf = [MaybeUninit::uninit(); 32];
+    let read = loop {
+      match mocket.recv_from(&mut buf) {
+        Ok((r, _)) => break r,
+        Err(e) if e.kind() == io::ErrorKind::WouldBlock => continue,
+        Err(e) => panic!("{e}"),
+      }
+    };
+    let serialized: Vec<u8> = buf[..read].iter().map(|b| unsafe { b.assume_init() }).collect();
+    
+    assert_eq!(serialized[0] as u32, ftel.messages_sent - 1);
+    assert_eq!(serialized[1], 0);
+    assert_eq!(serialized[2], 1);
+    panic!("Payload length: {}", u16::from_be_bytes(serialized[3..5].try_into().unwrap()));
   }
 }
