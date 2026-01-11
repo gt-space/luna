f7d01d71026466d546074061e204798ee83388cc
Amogh Rajagopal <rajagopal.amogh@gmail.com>
2026-01-10 23:26:12 -0800

    Fix for launch opcode

--- firmware/reco/src/lib.rs
+++ firmware/reco/src/lib.rs
@@ -41,9 +41,9 @@ const TOTAL_TRANSFER_SIZE: usize = RECO_BODY_SIZE;
 /// Opcodes for messages to RECO
 pub mod opcode {
     /// Opcode that tells RECO that the rocket has launched
-    pub const LAUNCHED: u8 = 0x01;
+    pub const LAUNCHED: u8 = 0x79;
     /// Opcode that sends RECO most recent GPS data and receives RECO telemetry
-    pub const GPS_DATA: u8 = 0x79;
+    pub const GPS_DATA: u8 = 0x02;
     /// Opcode requesting that RECO initialize (or reinitialize) its EKF.
     /// This repurposes the previous voting-logic opcode.
     pub const INIT_EKF: u8 = 0x03;
