ef4f9f62952313d7a715c6f739e198f14b3ea661
Diego Bonilla <diegob8846@gmail.com>
2026-02-22 19:27:44 -0500

    Finished rust workshop

---
+++ onboarding/sensor-fusion-sim/src/sensors.rs
@@ -0,0 +1,110 @@
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
+use tokio::time::{Duration, sleep};
+
+pub struct SensorReading {
+    pub name: String,
+    pub value: f64,
+    pub unit: String,
+}
+
+pub enum ValveState {
+    Open,
+    Closed,
+}
+
+trait Sensor {
+    fn read(&self) -> f64;
+}
+
+pub struct PressureSensor {
+    pub name: String,
+    pub pressure: f64,
+}
+
+impl Sensor for PressureSensor {
+    fn read(&self) -> f64 {
+        self.pressure
+    }
+}
+
+pub fn new_temp_sensor(name: String, value: f64) -> SensorReading {
+    SensorReading {
+        name,
+        value,
+        unit: "K".to_string(),
+    }
+}
+
+pub fn print_reading(sensor: &SensorReading) {
+    println!("{}, {}, {}", sensor.name, sensor.value, sensor.unit);
+}
+
+pub fn print_valve_state(state: &ValveState) {
+    match state {
+        ValveState::Open => println!("Valve is open"),
+        ValveState::Closed => println!("Valve is closed"),
+    }
+}
+
+pub fn safe_pressure(v: f64) -> Result<f64, String> {
+    if v < 0.0 {
+        Err("Negative pressure!".to_string())
+    } else {
+        Ok(v)
+    }
+}
+
+pub fn identity<T>(x: T) -> T {
+    x
+}
+
+pub fn maybe_sensor(active: bool) -> Option<f64> {
+    if active {
+        Some(300.0) // value exists
+    } else {
+        None // no value
+    }
+}
+
+pub async fn async_temp_sensor(name: &str, unit: &str) -> SensorReading {
+    let mut rng = StdRng::from_entropy(); // random number generator
+    let value: f64 = rng.gen_range(80.0..120.0);
+
+    sleep(Duration::from_millis(500)).await;
+
+    SensorReading {
+        name: name.to_string(),
+        value,
+        unit: unit.to_string(),
+    }
+}
+
+pub async fn async_pressure_sensor(name: &str, unit: &str) -> SensorReading {
+    let mut rng = StdRng::from_entropy();
+
+    let value: f64 = rng.gen_range(80.0..120.0);
+
+    sleep(Duration::from_millis(300)).await;
+
+    SensorReading {
+        name: name.to_string(),
+        value,
+        unit: unit.to_string(),
+    }
+}
+
+pub async fn async_valve_sensor(_name: &str) -> ValveState {
+    let mut rng = StdRng::from_entropy();
+
+    let state = if rng.gen_bool(0.5) {
+        // generate true with 0.5 probability
+        ValveState::Open
+    } else {
+        ValveState::Closed
+    };
+
+    sleep(Duration::from_millis(200)).await;
+
+    state
+}
