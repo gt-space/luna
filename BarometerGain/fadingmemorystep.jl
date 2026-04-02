c5c31b1851d4a0425a28ef9cd8904570cdefb6eb
VernierCorrections <161099687+VernierCorrections@users.noreply.github.com>
2026-04-02 19:26:39 -0400

    Update fadingmemorystep.jl
    Signed-off-by: VernierCorrections <161099687+VernierCorrections@users.noreply.github.com>

--- BarometerGain/fadingmemorystep.jl
+++ BarometerGain/fadingmemorystep.jl
@@ -17,7 +17,8 @@ function step_fmfs_o1!(x̂::Float64, x_meas::Float64, G::Float64)
     # a-priori step: do nothing because we assume the solution is constant in time in the absence of perturbations; x^-_k+1 = x^+_k
     # a-posteriori step: assume the system is influenced by first order perturbations, so compute the residual y_k = x_meas - x̂_k
     # multiply the residual by G and add to x^-_k+1 to get x^+_k+1
-    x̂ += G * (x_meas - x̂)
+    yₖ = x_meas - x̂
+    x̂ = muladd(G, yₖ, x̂)
 end
 
 
@@ -45,9 +46,10 @@ function step_fmfs_o2!(x̂::Float64, ẋ̂::Float64, x_meas::Float64, T_s::Float
     # a-posteriori step: assume the system is influenced by second order perturbations, so compute the residual y_k = x_meas - x^-_k+1
     # it follows then that the state derivative residual is ẏ_k = y_k / T_s
     # multiply both residuals by their respective gains and add to get x^+_k+1
-    y = x_meas - (x̂ + ẋ̂ * T_s)
-    x̂ += ẋ̂ * T_s + G * y
-    ẋ̂ += H * (y / T_s)
+    x_minus = muladd(ẋ̂, T_s, x̂)
+    yₖ = x_meas - x_minus
+    x̂ = muladd(G, yₖ, x̂)
+    ẋ̂ = muladd(h′, yₖ, ẋ̂)
 end
 
 
